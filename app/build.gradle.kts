781dfe35c0a6523c30c6b952842c1e10588b0b99
neoflorianvecn4 <noeflorian242003@gmail.com>
2024-06-05 22:15:22 -0300

    Autenticación por Correo Firebase (Funcional)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,10 @@ android {
 }
 
 dependencies {
+    //noinspection UseTomlInstead
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
